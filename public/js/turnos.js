550b28a53be16d248b81d1cd3fc0ebbd978c880c
Arturo Ávila Martínez <arturoavilamartinez9@gmail.com>
2025-10-15 23:25:53 -0600

    eliminar pdfs

---
+++ public/js/turnos.js
@@ -0,0 +1,14 @@
+document.getElementById("btnImprimirPDF").addEventListener("click", function() {
+    let pdfUrl = "generar_pdf.php";
+
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = pdfUrl; 
+
+    iframe.onload = function() {
+        iframe.contentWindow.focus();
+        iframe.contentWindow.print();
+    };
+
+    document.body.appendChild(iframe);
+});
