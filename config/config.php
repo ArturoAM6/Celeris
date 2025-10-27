61d3b185a585c57bc9cde26489a5188f2359a903
Arturo Ávila Martínez <arturoavilamartinez9@gmail.com>
2025-10-27 09:58:33 -0600

    credenciales bd

--- config/config.php
+++ config/config.php
@@ -14,8 +14,8 @@ if (ENTORNO_DESARROLLO) {
 }
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'celeris_user');
+define('DB_PASS', '123456');
 define('DB_NAME', 'celeris');
 
 define('BASE_URL', '/Celeris/public');
